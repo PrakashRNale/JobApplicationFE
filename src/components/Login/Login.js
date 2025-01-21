dcd764adb20bdfa79f1eccabe1d365ecd47f5f16
PrakashRN <prakashcsedyp@gmail.com>
2025-01-21 19:20:54 +0530

    Final working code after deployment

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,32 +7,18 @@ import UserContext from '../../context/UserContext/UserContext';
 const Login = ({setShowLogin}) => {
     const userContext = useContext(UserContext);
     console.log(process.env)
-    const handleLoginSuccess = async (response) => {
-        console.log('Google Token:', response.credential);
-        localStorage.setItem("token", response.credential)
-        try {
-            // Send the token to the backend to validate and fetch user info
-            const res = await axios.post('http://localhost:8000/auth/google/token', 
-                { token: response.credential },
-                { withCredentials: true }  // To include cookies for sessions if needed
-            );
-            userContext.login(res.data.user);  // Passing user data back to parent component
-            setShowLogin(false);
-        } catch (error) {
-            console.error('Error:', error);
-        }
+    const handleRedirectLogin = () => {
+        console.log(process.env.REACT_APP_API_ENDPOINT+'/auth/google');
+        window.location.href = process.env.REACT_APP_API_ENDPOINT+'/auth/google'; // Redirect to your backend
     };
+    
 
     return (
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-        <div className=''>
-            <GoogleLogin
-                onSuccess={handleLoginSuccess}
-                onError={() => console.error('Google Login Failed')}
-                text="signin_with"
-            />
+        <div>
+            <button onClick={handleRedirectLogin}>
+                Sign in with Google
+            </button>
         </div>
-        </GoogleOAuthProvider>
     );
 };
 
